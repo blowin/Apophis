88fe8cb6ac52a6d0b5615ef6f70e3aeb14b6b401
blowin <dsessence@gmail.com>
2018-08-28 09:19:03 +0300

    Update OptionExtensions.cs

--- Apophis/Types/Monads/Option/OptionExtensions.cs
+++ Apophis/Types/Monads/Option/OptionExtensions.cs
@@ -2,6 +2,6 @@ namespace FunctionalProgramming.Apophis.Types.Monads.Option
 {
     public static class OptionalExtensions
     {
-        public static Option<T> ToOption<T>(this T obj) => new Option<T>(obj);
+        public static Option<T> ToOption<T>(this T obj){ return new Option<T>(obj); }
     }
 }
