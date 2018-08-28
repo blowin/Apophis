6da094bb60d7ab74dab6c32df4ddaa8dede5679f
blowin <dsessence@gmail.com>
2018-08-28 09:19:22 +0300

    Update OptionalExceptions.cs

--- Apophis/Types/Monads/Option/OptionalExceptions.cs
+++ Apophis/Types/Monads/Option/OptionalExceptions.cs
@@ -4,7 +4,22 @@ namespace FunctionalProgramming.Apophis.Types.Monads.Option
 {
     public static class OptionalExceptions
     {
-        public static void NotFount() => throw new OptionalNotFoundValue();
+        public static void NotFount()
+        {
+            throw new OptionalNotFoundValue();
+        }
+
+        public static void NullHandler(string msg = "Function for handler not be null")
+        {
+            throw new ArgumentNullException(msg);
+        }
+
+        public static void NullHandlerCheck<T>(T func, string msg = "Function for handler not be null")
+            where T : class
+        {
+            if(func == null)
+                NullHandler();
+        }
         
         public sealed class OptionalNotFoundValue : Exception{}
     }
